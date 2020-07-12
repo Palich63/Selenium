64b4233220efddb7c263ddbea3e08f055b0c2f54
Andrey Globa <Globa.Andrey@gmail.com>
2020-07-12 23:25:37 +0300

    experiment

--- src/test/java/ru/netology/TestOrderCard.java
+++ src/test/java/ru/netology/TestOrderCard.java
@@ -12,33 +12,33 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestOrderCard {
-    private WebDriverRunner driver;
-
-    @BeforeAll
-    static void setUpAll() {
-
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-    }
-
-    @BeforeEach
-    void setUp() {
-        driver = new WebDriverRunner();
-    }
-
-    @AfterEach
-    void tearDown() {
-
-        driver = null;
-    }
-
-    @Test
-    void shouldTestOrderCard() {
-        open("http://localhost:9999");
-        SelenideElement form = $("[method='post']");
-        form.$("[name='name']").setValue("Андреев");
-        form.$("[type='tel']").setValue("+79998887755");
-        form.$("[data-test-id='agreement']").click();
-        form.$("[type='button']").click();
-        $("[data-test-id='order-success']").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
-    }
+//    private WebDriverRunner driver;
+//
+//    @BeforeAll
+//    static void setUpAll() {
+//
+//        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+//    }
+//
+//    @BeforeEach
+//    void setUp() {
+//        driver = new WebDriverRunner();
+//    }
+//
+//    @AfterEach
+//    void tearDown() {
+//
+//        driver = null;
+//    }
+//
+//    @Test
+//    void shouldTestOrderCard() {
+//        open("http://localhost:9999");
+//        SelenideElement form = $("[method='post']");
+//        form.$("[name='name']").setValue("Андреев");
+//        form.$("[type='tel']").setValue("+79998887755");
+//        form.$("[data-test-id='agreement']").click();
+//        form.$("[type='button']").click();
+//        $("[data-test-id='order-success']").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+//    }
 }
