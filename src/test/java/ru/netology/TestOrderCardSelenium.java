9c3ce2fbdff73bb69a618c9d3d49ded82845d7b6
Andrey Globa <Globa.Andrey@gmail.com>
2020-07-12 22:57:18 +0300

    experiment

--- src/test/java/ru/netology/TestOrderCardSelenium.java
+++ src/test/java/ru/netology/TestOrderCardSelenium.java
@@ -12,33 +12,33 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestOrderCardSelenium {
-//    private WebDriver driver;
-//
-//    @BeforeAll
-//    static void setUpAll() {
-//        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-//    }
-//
-//    @BeforeEach
-//    void setUp() {
-//        driver = new ChromeDriver();
-//    }
-//
-//    @AfterEach
-//    void tearDown() {
-//        driver.quit();
-//        driver = null;
-//    }
-//
-//    @Test
-//    void shouldTestOrderCardSelenium() {
-//        driver.get("http://localhost:9999");
-//        WebElement form = driver.findElement(By.cssSelector("[method=post]"));
-//        form.findElement(By.cssSelector("[name=name]")).sendKeys("Андрей");
-//        form.findElement(By.cssSelector("[name=phone]")).sendKeys("+79998887755");
-//        form.findElement(By.cssSelector("[class=checkbox__box]")).click();
-//        form.findElement(By.cssSelector("[class=button__content]")).click();
-//        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
-//        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
-//    }
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTestOrderCardSelenium() {
+        driver.get("http://localhost:9999");
+        WebElement form = driver.findElement(By.cssSelector("[method=post]"));
+        form.findElement(By.cssSelector("[name=name]")).sendKeys("Андрей");
+        form.findElement(By.cssSelector("[name=phone]")).sendKeys("+79998887755");
+        form.findElement(By.cssSelector("[class=checkbox__box]")).click();
+        form.findElement(By.cssSelector("[class=button__content]")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
 }
