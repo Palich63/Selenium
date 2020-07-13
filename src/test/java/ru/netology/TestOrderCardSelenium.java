6c6025d07750dbf694ca85d02ad51556a4f4c6b1
Andrey Globa <Globa.Andrey@gmail.com>
2020-07-13 16:23:21 +0300

    experiment

--- src/test/java/ru/netology/TestOrderCardSelenium.java
+++ src/test/java/ru/netology/TestOrderCardSelenium.java
@@ -10,12 +10,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestOrderCardSelenium {
-    private WebDriver driver;
+    private ChromeDriver driver;
     private static ChromeOptions options;
 
     @BeforeAll
